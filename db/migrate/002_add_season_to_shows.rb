d3ec689131d4f044ebab3bebe917ccc26cbde312
Audrey Jones <audrey.eliz.jones@gmail.com>
2019-03-26 17:57:21 +0000

    Automatically backed up by Learn

--- db/migrate/002_add_season_to_shows.rb
+++ db/migrate/002_add_season_to_shows.rb
@@ -2,9 +2,4 @@ class AddSeasonToShows < ActiveRecord::Migration
   def change
       add_column :shows, :season, :string
   end
-
-  def highest_rating
-    binding.pry
-    Show.maximum(:rating)
-  end
 end
